971bff869a0ce413b9878fe238bdb62ec9e44770
Mena Aziz <menaaziz27@gmail.com>
2022-07-21 15:40:25 +0200

    saving data in csv and json

--- myscraper/items.py
+++ myscraper/items.py
@@ -6,7 +6,7 @@
 import scrapy
 
 
-class MyscraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class QuoteItem(scrapy.Item):
+    text = scrapy.Field()
+    author = scrapy.Field()
+    tags = scrapy.Field()
--- myscraper/spiders/goodreads.py
+++ myscraper/spiders/goodreads.py
@@ -1,3 +1,5 @@
+from scrapy.loader import ItemLoader
+from items import QuoteItem
 import scrapy
 
 class GoodReadsSpider(scrapy.Spider):
@@ -5,26 +7,23 @@ class GoodReadsSpider(scrapy.Spider):
     name = 'goodreads'
     # requests
     def start_requests(self):
-        urls = [
-            'https://www.goodreads.com/quotes?page=1',
-            'https://www.goodreads.com/quotes?page=2',
-            'https://www.goodreads.com/quotes?page=3',
-            'https://www.goodreads.com/quotes?page=4',
-            'https://www.goodreads.com/quotes?page=5',
-            'https://www.goodreads.com/quotes?page=6',
-            'https://www.goodreads.com/quotes?page=7',
-            'https://www.goodreads.com/quotes?page=8',
-            'https://www.goodreads.com/quotes?page=9',
-            'https://www.goodreads.com/quotes?page=10',
-        ]
-
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        url = 'https://www.goodreads.com/quotes?page=1'
+        
+        yield scrapy.Request(url=url, callback=self.parse)
 
     
      # response
     def parse(self, response):
-        page_number = response.url.split('=')[1]
-        _file = f'${page_number}.html'
-        with open(_file, 'wb') as f: 
-            f.write(response.body)
+        for quote in response.selector.xpath("//div[@class='quote']"):
+            yield {
+                'text': quote.xpath(".//div[@class='quoteText'][1]/text()").extract_first(),
+                'author': quote.xpath(".//div[@class='quoteText']/child::span/text()").extract_first(),
+                'tags': quote.xpath(".//div[@class='greyText smallText left']/child::a/text()").extract(),
+            }
+
+        next_page= response.selector.xpath("//a[@class='next_page']/@href").extract_first()
+
+        if next_page is not None:
+            next_page_link = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page_link, callback=self.parse)
+        
